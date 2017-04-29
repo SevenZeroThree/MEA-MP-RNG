e9d9a5bf4a65771de6c9708d84ae96980e290fd4
Chris Tobin <hockey73god@yahoo.com>
2017-04-29 16:55:32 -0400

    Add Twitter component

---
+++ components/Twitter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Twitter = () => (
+    <div>
+        <a href="https://twitter.com/sevenzer0three" target="_blank">Twitter</a>
+    </div>
+)
+
+export default Twitter;
