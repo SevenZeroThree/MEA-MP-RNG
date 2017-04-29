5715494b77538ef244cdcc216ae15183a719a499
Chris Tobin <hockey73god@yahoo.com>
2017-04-29 16:57:14 -0400

    Add Github component

---
+++ components/Github.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Github = () => (
+    <a href="https://github.com/sevenzerothree" target="_blank">github.com/sevenzerothree</a>
+)
+
+export default Github;
--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Twitter from './Twitter';
+import Github from './Github';
 
 const SocialLinks = () => (
     <div className="social">
         <Twitter />
+        <Github />
     </div>
 )
 
