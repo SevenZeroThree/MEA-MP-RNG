291b316037cad76e1164ab7572e8f2d94a358bfc
Chris Tobin <hockey73god@yahoo.com>
2017-04-29 16:58:46 -0400

    remove wrapper div in Twitter component

--- components/Twitter.js
+++ components/Twitter.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Twitter = () => (
-    <div>
-        <a href="https://twitter.com/sevenzer0three" target="_blank">Twitter</a>
-    </div>
+    <a href="https://twitter.com/sevenzer0three" target="_blank">Twitter</a>
 )
 
 export default Twitter;
