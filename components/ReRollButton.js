0bc4a6c37af25b10a00012f8f870a8247928dad1
Chris Tobin <hockey73god@yahoo.com>
2017-05-03 19:40:35 -0400

    Install prop-types package

--- components/ReRollButton.js
+++ components/ReRollButton.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const ReRollButton = ({onClick}) => (
     <button id="re-roll-button" className="btn btn-default" onClick={onClick}>ReRoll</button>
